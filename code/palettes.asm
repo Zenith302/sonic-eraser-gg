61bebcadca018dfe04c7a9ac1da73e88a24dabc7
ZenithNeko <micklepickle360@gmail.com>
2025-02-26 08:21:33 -0500

    add the gameplay palette

--- code/palettes.asm
+++ code/palettes.asm
@@ -53,7 +53,7 @@ pal_intro:
     db      &55, &05
     db      &33, &03
 
-    db      &00, &00    ; palette 1
+    db      &F0, &0F    ; palette 1
     db      &00, &00
     db      &00, &00
     db      &00, &00
@@ -69,3 +69,38 @@ pal_intro:
     db      &0C, &00
     db      &00, &00
     db      &00, &00
+
+pal_ingame:
+    db      &B9, &0B    ; palette 0
+    db      &86, &08
+    db      &64, &06
+    db      &42, &04
+    db      &20, &02
+    db      &00, &00
+    db      &F0, &05
+    db      &A0, &03
+    db      &50, &00
+    db      &FF, &0F
+    db      &DD, &0D
+    db      &BB, &0B
+    db      &99, &09
+    db      &77, &07
+    db      &55, &05
+    db      &33, &03
+
+    db      &F0, &0F    ; palette 1
+    db      &00, &00
+    db      &3F, &00
+    db      &29, &00
+    db      &BF, &06
+    db      &5A, &00
+    db      &A0, &03
+    db      &50, &00
+    db      &34, &0E
+    db      &13, &0B
+    db      &9E, &05
+    db      &99, &0F
+    db      &55, &0F
+    db      &FF, &0F
+    db      &BB, &0B
+    db      &55, &05
